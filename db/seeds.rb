244082c263985f89f63ff32c8dc3d597b92a467c
Fred Duby <fcd1@columbia.edu>
2016-10-11 12:50:59 -0400

    Add controller, views and routes for user. Seed admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create(email: "admin@example.com",
+                         password: "admin1234",
+                         password_confirmation: "admin1234")
